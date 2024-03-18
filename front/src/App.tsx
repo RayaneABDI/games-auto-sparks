ae919d7e1b19022c8582b6db37dcfc6dcfeec86e
RayaneABDI <rayane.abdi@akur8.com>
2024-03-18 19:20:26 +0900

    setup dependenciews

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Apphtml = ``
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>``
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
