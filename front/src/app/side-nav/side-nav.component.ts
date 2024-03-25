ea17532d5d351857b789a164be5f66e5f4cc3b18
RayaneABDI <m.rayaneabdi@gmail.com>
2024-03-25 16:28:17 +0900

    - GO TO ANGULAR 17

--- front/src/app/side-nav/side-nav.component.ts
+++ front/src/app/side-nav/side-nav.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
   imports: [
-    MatSidenavModule,MatButtonModule
+    MatSidenavModule
   ],
   standalone: true
 })
