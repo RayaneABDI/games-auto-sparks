dfcfd9ba5e6dcd54ec8dcdc87ed8acc820bb2b7d
RayaneABDI <m.rayaneabdi@gmail.com>
2024-03-28 00:12:39 +0900

    - angular trying

--- front/src/app/side-nav/side-nav.component.ts
+++ front/src/app/side-nav/side-nav.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIcon} from "@angular/material/icon";
+import {NgOptimizedImage} from "@angular/common";
+import {MatListItem, MatNavList} from "@angular/material/list";
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
   imports: [
-    MatSidenavModule
+    MatSidenavModule,
+    MatIcon,
+    NgOptimizedImage,
+    MatNavList,
+    MatListItem
   ],
   standalone: true
 })
